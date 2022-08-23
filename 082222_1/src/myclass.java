2205b3225520ae3002bde90f9da4cc9f0de199f5
TrooperZ <trooperz.tz@gmail.com>
2022-08-22 20:52:41 -0500

    082222_pd6

---
+++ 082222_1/src/myclass.java
@@ -0,0 +1,9 @@
+public class myclass {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            System.out.println("Rhinos! Welcome to AP class!");
+
+        } //end of main
+
+}
