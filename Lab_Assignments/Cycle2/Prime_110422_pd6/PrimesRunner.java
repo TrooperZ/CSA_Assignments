ad9ff27be1ae2cbadc93b92750bbf6dee2477830
TrooperZ <trooperz.tz@gmail.com>
2022-12-01 10:39:12 -0600

    Reorganized Repo

--- Lab_Assignments/Cycle2/Prime_110422_pd6/PrimesRunner.java
+++ Lab_Assignments/Cycle2/Prime_110422_pd6/PrimesRunner.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class PrimesRunner
 {
     public static void main( String args[] ) throws FileNotFoundException {
-        File text = new File("C:\\Users\\troop\\IdeaProjects\\Prime_AminKaric_110422_pd6\\src\\primes.txt");
-        // File text = new File("primes.txt"); // doesnt work on my pc so i used the upper one
+        File text = new File("Your\File\Here\");
 
         //Creating Scanner instance to read File in Java
         Scanner s = new Scanner(text);
