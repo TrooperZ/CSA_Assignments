a415b3b137a1a842891f36f802f4f6447ac95c43
TrooperZ <trooperz.tz@gmail.com>
2022-12-05 15:34:49 -0600

    Update ListDown.java

--- Lab_Assignments/Cycle3/ListDown/ListDown.java
+++ Lab_Assignments/Cycle3/ListDown/ListDown.java
@@ -12,8 +12,8 @@ public class ListDown
     //go() will return true if all numbers in numArray
     //are in decreasing order [31,12,6,2,1]
     public static boolean go( List<Integer> numArray)
-    {
-        if (numArray.stream().distinct().count() <= 1 && numArray.size() != 1) {
+    {                                                                             // checks if list stays the same
+        if (numArray.stream().distinct().count() <= 1 && numArray.size() != 1) {  //converts the arraylist to a byte stream, gets distinct elements, counts if there are 1 or less
             return false;
         }
         List<Integer> newarr = new ArrayList<Integer>(numArray);
