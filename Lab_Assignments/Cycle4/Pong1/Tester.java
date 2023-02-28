d846fb9f65565620660f791c6488b1d28ae28b32
TrooperZ <trooperz.tz@gmail.com>
2023-02-28 10:09:30 -0600

    Update Pong

---
+++ Lab_Assignments/Cycle4/Pong1/Tester.java
@@ -0,0 +1,35 @@
+package Pong1;//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+ 
+import javax.swing.JFrame;
+
+public class Tester extends JFrame
+{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+
+	public Tester()
+	{
+		super("PONG TESTER");
+		setSize(WIDTH,HEIGHT);
+
+		getContentPane().add(new BlockTestTwo());
+
+		//uncomment when you are ready to test the Ball
+		//getContentPane().add(new BallTestTwo());
+
+		//PaddleTestTwo padTest = new PaddleTestTwo();
+		//((Component)padTest).setFocusable(true);
+		//getContentPane().add(padTest);
+
+		setVisible(true);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main( String args[] )
+	{
+		Tester run = new Tester();
+	}
+}
